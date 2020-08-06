13c152f4052979a7ff7b629f324cfa7ea8586219
Marina Nakagawa <mari10172001@yahoo.co.jp>
2020-08-06 15:08:12 +0900

    testtetsetをいれた

--- test/handler.js
+++ test/handler.js
@@ -8,6 +8,8 @@ module.exports.hello = async event => {
         message: 'Go Serverless v1.0! Your function executed successfully!',
         input: event,
       },
+      tetstteteete
+      
       null,
       2
     ),
